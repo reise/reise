04ea974c1828f32aa29c57c5b18fc6ec3ca55eff
Krish <shubhamdigole@gmail.com>
2018-03-02 17:48:05 +0530

    Added Initial project structure

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginPageComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
