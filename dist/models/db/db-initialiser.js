a178b6d581b5320d16e5a07598735dd7f24bcecc
Manoj Chalode <chalodem@gmail.com>
2018-03-29 23:42:12 +0100

    gh-pages - added mongo db connection string

--- dist/models/db/db-initialiser.js
+++ dist/models/db/db-initialiser.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
-function connect(url = "mongodb://localhost/reise") {
+function connect(url = "mongodb://manojc:1qazZAQ!QAZ@cluster0-shard-00-00-utnpu.mongodb.net:27017,cluster0-shard-00-01-utnpu.mongodb.net:27017,cluster0-shard-00-02-utnpu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin") {
     return new Promise((resolve, reject) => {
         mongoose_1.connect(url, ((err) => {
             if (err) {
