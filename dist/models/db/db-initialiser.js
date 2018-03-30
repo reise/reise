c405564e40b7f9405fb61c3189292cd9641b80d5
Manoj Chalode <chalodem@gmail.com>
2018-03-30 08:02:42 +0100

    Update db-initialiser.js

--- dist/models/db/db-initialiser.js
+++ dist/models/db/db-initialiser.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
-function connect(url = "mongodb+srv://manoj:12345@cluster0.mongodb.net/test") {
+function connect(url = "mongodb://manoj:12345@mycluster0-shard-00-00.mongodb.net:27017,mycluster0-shard-00-01.mongodb.net:27017,mycluster0-shard-00-02.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin") {
     return new Promise((resolve, reject) => {
         mongoose_1.connect(url, ((err) => {
             if (err) {
