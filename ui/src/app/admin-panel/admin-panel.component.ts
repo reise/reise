6777d50ae0924df6497c63b44dbc3ba5b416084c
webuilderkrish <shubhamdigole@gmail.com>
2018-04-06 02:17:14 +0530

    master - fixed admin panel url bug

--- ui/src/app/admin-panel/admin-panel.component.ts
+++ ui/src/app/admin-panel/admin-panel.component.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AdminDataService } from '../admin-data.service';
-
+import { User } from '../models/user-model';
 @Component({
     selector: 'app-admin-panel',
     templateUrl: './admin-panel.component.html',
@@ -13,16 +13,19 @@ import { AdminDataService } from '../admin-data.service';
 export class AdminPanelComponent implements OnInit {
 
     public data:Array<any>= [];
-
+    public user;
    
 
   
  
-  constructor(private _AdminDataService: AdminDataService) { }
+  constructor(private _AdminDataService: AdminDataService,private _Router: Router) { }
     
   public ngOnInit(): void {
     this._AdminDataService.getData().subscribe((response: any) => this.data = JSON.parse(JSON.stringify(response.data)));
-  
+    this.user = JSON.parse(sessionStorage.getItem('user'));
+    if (!this.user || !this.user.isAdmin) {
+        this._Router.navigate(['/login']);
+    }
     }
  
 
