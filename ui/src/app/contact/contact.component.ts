8db340cd64fb0b132d360b475196741f8159a747
Manoj Chalode <chalodem@gmail.com>
2018-04-15 22:59:19 +0100

    master - removed authentication from home and about components.

--- ui/src/app/contact/contact.component.ts
+++ ui/src/app/contact/contact.component.ts
@@ -3,20 +3,13 @@ import { Router } from '@angular/router';
 import { User } from '../models/user-model';
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+    selector: 'app-contact',
+    templateUrl: './contact.component.html',
+    styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-user:User;
-  constructor(private _Router:Router) { }
+    constructor(private _Router: Router) { }
 
-  ngOnInit() {
-    this.user = JSON.parse(sessionStorage.getItem('user'));
-    if (!this.user || !this.user.username) {
-        this._Router.navigate(['/login']);
+    public ngOnInit(): void {
     }
-
-  }
-
 }
